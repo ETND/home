45fe8b500ab4e945174aa8a2f752d6fa15e1fb7e
ETND <admin@etnd.pro>
2022-06-16 14:16:40 +0800

    Up
    Up

---
+++ js/shouye.js
@@ -0,0 +1,183 @@
+new Swiper(".loop", {
+  // 垂直切换选项
+  direction: "horizontal",
+  // 循环模式选项
+  loop: true,
+  pauseOnMouseEnter: true,
+  autoplay: {
+    delay: 1000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+  },
+  // 如果需要分页器
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+								var qb = document.getElementById("qb");
+								var wk = document.getElementById("wk");
+								var de = document.getElementById("de");
+								var yx = document.getElementById("yx");
+								var jr = document.getElementById("jr");
+								var jy = document.getElementById("jy");
+								var div1 = document.getElementById("div1");
+								var div2 = document.getElementById("div2");
+								var div3 = document.getElementById("div3");
+								var div4 = document.getElementById("div4");
+								var div5 = document.getElementById("div5");
+								var div6 = document.getElementById("div6");
+								
+								qb.onclick = function () {
+												qb.style.color = "#000"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+								    div1.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								}
+								wk.onclick = function () {
+												qb.style.color = "#CCCCCC"
+												wk.style.color = "#000"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+								    div4.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div1.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								}
+								de.onclick = function () {
+												qb.style.color = "#CCCCCC"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#000"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+								    div3.style.display = "block";
+												div2.style.display = "none";
+												div1.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								}
+								yx.onclick = function () {
+												qb.style.color = "#CCCCCC"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#000"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+								    div6.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div1.style.display = "none";
+								}
+								jr.onclick = function () {
+												qb.style.color = "#CCCCCC"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#000"
+												jy.style.color = "#CCCCCC"
+								    div5.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div6.style.display = "none";
+												div1.style.display = "none";
+								}
+								jy.onclick = function () {
+												qb.style.color = "#CCCCCC"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#000"
+								    div2.style.display = "block";
+												div1.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								}
+								var jpg = document.getElementById("jpq");
+								var mb = document.getElementById("mb");
+								var tab = document.getElementById("tab");
+								jpg.onclick = function () {
+								    mb.style.display = "block";
+												tab.style.display = "none";
+								}
+								mb.onclick = function () {
+								    mb.style.display = "none";
+												tab.style.display = "block";
+												qb.style.color = "#000"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+								    div1.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								
+								}
+								var rdq = document.getElementById("rdq");
+								var mb1 = document.getElementById("mb1");
+								
+								rdq.onclick = function () {
+								    mb1.style.display = "block";
+												tab.style.display = "none";
+								}
+								mb1.onclick = function () {
+								    mb1.style.display = "none";
+												tab.style.display = "block";
+												qb.style.color = "#000"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+												div1.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								}
+								var sc = document.getElementById("sc");
+								var mb2 = document.getElementById("mb2");
+								
+								sc.onclick = function () {
+								    mb2.style.display = "block";
+												tab.style.display = "none";
+								}
+								mb2.onclick = function () {
+								    mb2.style.display = "none";
+												tab.style.display = "block";
+												qb.style.color = "#000"
+												wk.style.color = "#CCCCCC"
+												de.style.color = "#CCCCCC"
+												yx.style.color = "#CCCCCC"
+												jr.style.color = "#CCCCCC"
+												jy.style.color = "#CCCCCC"
+								    div1.style.display = "block";
+												div2.style.display = "none";
+												div3.style.display = "none";
+												div4.style.display = "none";
+												div5.style.display = "none";
+												div6.style.display = "none";
+								}
